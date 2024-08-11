f77edc17947e00c2b24c226ceabafc260a073c3d
shymantr83 <144794315+shymantr83@users.noreply.github.com>
2024-08-11 15:21:08 +0300

    Add files via upload

---
+++ lang/en/words.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'Home' => 'Home ',
+    'specialisation' => 'specialisation',
+    'About'=>'About ',
+    'language'=>' language',
+    'Contact'=>'Contact',
+    'Profile'=>'Profile ',
+    'login'=>'login',
+    'register'=>'register ',
+    'user'=>'User',
+    'company'=>'Company ',
+    'logout'=>'logout ',
+    'footer'=>'smart guide for engineering office we help you reach the best '
+
+
+];
+
